038c8603b0672f7a548e448427e559b663cc7fb0
中田卓郎 <takurou403@gmail.com>
2021-06-12 03:17:49 +0900

    Route Tableリソース定義を設定

--- network.tf
+++ network.tf
@@ -68,3 +68,40 @@ resource "aws_subnet" "private_subnet_1c" {
     Type    = "private"
   }
 }
+
+# route tableリソース設定
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-public_root_table"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "public"
+  }
+}
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-private_root_table"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "public"
+  }
+}
+# route table 関連付けリソース設定
+resource "aws_route_table_association" "public_rt_1a" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+}
+resource "aws_route_table_association" "public_rt_1c" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+}
+resource "aws_route_table_association" "private_rt_1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+}
+resource "aws_route_table_association" "private_rt_1c" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
+}
