82cab0bbf0694a40f71d2d73ab3401d1c2f8e7f1
中田卓郎 <takurou403@gmail.com>
2021-06-11 11:26:51 +0900

    nginxスクリプトインストールを定義

--- main.tf
+++ main.tf
@@ -9,4 +9,12 @@ resource "aws_instance" "hello-world" {
 	ami = "ami-001f026eaf69770b4"
 	instance_type = "t2.micro"
 
+	tags = {
+		Name = "HelloWorld" 
+	}
+	user_data = <<EOF
+	#!/bin/bash
+	amazon-linux-extras install -y nginx
+	systemctl start nginx
+	EOF
 }
