1b30fdd821c7d5d458cf1fb813db38c61ed67f4f
中田卓郎 <takurou403@gmail.com>
2021-06-13 08:40:56 +0900

    IAM ロール定義を作成

--- iam.tf
+++ iam.tf
@@ -1,5 +1,12 @@
 # IAM configration
 
+# IAM Role
+resource "aws_iam_role" "app_iam_role" {
+  name = "${var.project}-${var.enviroment}-app-iam-role"
+  # 紐付けるデータ本体:信頼ポリシーのjsonオブジェクト
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+
+}
 # IAM Role 信頼ポリシー
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
@@ -12,3 +19,4 @@ data "aws_iam_policy_document" "ec2_assume_role" {
     }
   }
 }
+
