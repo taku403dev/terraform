f5abe031f80c9d78cdba6d6a64df7bbf4bcf96f0
中田卓郎 <takurou403@gmail.com>
2021-06-12 12:15:40 +0900

    RDSインスタンス設定を作成

--- rds.tf
+++ rds.tf
@@ -1,4 +1,4 @@
-# RDSリソース設定
+# RDSパラメータグループ設定
 
 resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
   name   = "${var.project}-${var.enviroment}-mysql-standalone-parametergroup"
@@ -14,7 +14,7 @@ resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
   }
 }
 
-# RDS option group
+# RDSオプショングループ設定
 
 resource "aws_db_option_group" "mysql_standalone_optiongroup" {
   name                 = "${var.project}-${var.enviroment}-mysql-standalone-optiongroup"
@@ -22,7 +22,7 @@ resource "aws_db_option_group" "mysql_standalone_optiongroup" {
   major_engine_version = "8.0"
 }
 
-resource "aws_db_subnet_group" "mysql_standalone_group" {
+resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
   name = "${var.project}-${var.enviroment}-mysql-standalone-group"
   subnet_ids = [
     aws_subnet.private_subnet_1a.id,
@@ -34,4 +34,57 @@ resource "aws_db_subnet_group" "mysql_standalone_group" {
     Env     = var.enviroment
   }
 
+}
+
+# RDSインスタンス設定
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.20"
+  identifier     = "${var.project}-${var.enviroment}-mysql-standalone"
+  username       = "admin"
+  password       = random_string.db_password.result
+  #   インスタンス設定
+  instance_class = "db.t2.micro"
+  #   ストレージ設定
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_type          = "gp2"
+  storage_encrypted     = false
+  # ネットワーク設定
+  multi_az          = false
+  availability_zone = "ap-northeast-1a"
+  # サブネット指定
+  db_subnet_group_name = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [
+    aws_security_group.db_sg.id
+  ]
+  publicly_accessible = false
+  port                = 3306
+
+  # データベース名
+  name                 = "tastylog"
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+
+  # バックアップを先に実行してからメンテナンスを実行する設定
+  # バックアップを実行する時間帯指定
+  backup_window = "04:00-05:00"
+  # バックアップを保管する期間の日付指定
+  backup_retention_period = 7
+  # メンテナンス日時を指定
+  maintenance_window = "Mon:05:00-Mon:08:00"
+  # バージョンを自動でアップグレード設定
+  auto_minor_version_upgrade = false
+  # 削除保護設定
+  deletion_protection = false
+  #   スナップショット削除時にスナップショット作成設定
+  skip_final_snapshot = true
+
+  # 即時作成設定
+  apply_immediately = true
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-mysql-standalone"
+    Project = var.project
+    Env     = var.enviroment
+  }
 }
