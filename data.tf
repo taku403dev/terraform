c4bf6db0d0a2e7a868ef6ad8cdad4d8081533e69
中田卓郎 <takurou403@gmail.com>
2021-06-12 14:22:57 +0900

    EC2 AMIリソース取り込み設定を作成

--- data.tf
+++ data.tf
@@ -4,3 +4,24 @@
 data "aws_prefix_list" "s3_p1" {
   name = "com.amazonaws.*.s3"
 }
+
+# AMIを検索して取り込む設定
+# aws cli 下記コマンド利用して検索
+# aws ec2 describe-images --image-ids イメージIDを指定
+data "aws_ami" "app" {
+  most_recent = true
+  owners      = ["self", "amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
