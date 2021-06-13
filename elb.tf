a110c823e12b70e15e7308b2a53c8d255c16fc1e
中田卓郎 <takurou403@gmail.com>
2021-06-14 04:50:44 +0900

    ELB ターゲットグループを作成

--- elb.tf
+++ elb.tf
@@ -11,3 +11,22 @@ resource "aws_lb" "alb" {
     aws_subnet.public_subnet_1c.id
   ]
 }
+
+# target group
+resource "aws_alb_target_group" "alb_target_group" {
+  name     = "${var.project}-${var.enviroment}-app-tg"
+  port     = "3000"
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-app-tg"
+    Project = var.project
+    Env     = var.enviroment
+  }
+}
+
+# ターゲットグループとEC2インスタンスの紐付け
+resource "aws_lb_target_group_attachment" "instance" {
+  target_group_arn = aws_alb_target_group.alb_target_group.arn
+  target_id        = aws_instance.app_server.id
+}
