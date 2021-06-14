46e2d8d6648504e9c7b201d788e4089848dbd101
中田卓郎 <takurou403@gmail.com>
2021-06-14 17:47:09 +0900

    ACMを作成

---
+++ acm.tf
@@ -0,0 +1,19 @@
+# ACM
+
+resource "aws_acm_certificate" "tokyo_cert" {
+  domain_name       = "*.${var.domain}"
+  validation_method = "DNS"
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-wildcard-sslcert"
+    Project = var.project
+    ENV     = var.enviroment
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+  depends_on = [
+    aws_route53_zone.route53_zone
+  ]
+}
+
