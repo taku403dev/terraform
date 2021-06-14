443a5b6b0a3a1d7ed5c4f9a2de7f81974870953d
中田卓郎 <takurou403@gmail.com>
2021-06-14 17:01:53 +0900

    ELB lintenerバグ修正

--- elb.tf
+++ elb.tf
@@ -15,7 +15,7 @@ resource "aws_lb" "alb" {
 # target group
 resource "aws_alb_target_group" "alb_target_group" {
   name     = "${var.project}-${var.enviroment}-app-tg"
-  port     = "3000"
+  port     = var.app_port
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
   tags = {
@@ -28,8 +28,8 @@ resource "aws_alb_target_group" "alb_target_group" {
 # Elb listener
 resource "aws_lb_listener" "alb_listener_http" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = 80
-  protocol          = "http"
+  port              = var.web_port
+  protocol          = "HTTP"
   default_action {
     type             = "forward"
     target_group_arn = aws_alb_target_group.alb_target_group.arn
